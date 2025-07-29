b0445a7255e435d86e58717bebf0af32cec3e7c7
afaqmvirk <afaq.virk135@outlook.com>
2025-07-28 23:41:32 -0400

    fixed centering issues

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -29,18 +29,18 @@ export default function Contact() {
         <motion.a
           initial={{ opacity: 0, scale: 1 }}
           animate={{ opacity: 1, scale: 1 }}
-          className="md:text-5xl text-3xl font-bold bg-[#052829]  p-4 rounded-2xl"
+          className="md:text-5xl text-3xl font-bold bg-[#052829] p-4 rounded-2xl block mx-auto"
           href="mailto:afaq@mathematech.ca"
         >
           👉 afaq@mathematech.ca
         </motion.a>
-        <div className=" flex space-x-6 mt-[5rem] mb-20">
+        <div className="flex space-x-6 mt-[5rem] mb-20 justify-center">
           <SocialBar />
         </div>
         <motion.a
           initial={{ opacity: 0, scale: 1 }}
           animate={{ opacity: 1, scale: 1 }}
-          className="md:text-3xl text-2xl font-extrabold underline rounded-2xl"
+          className="md:text-3xl text-2xl font-extrabold underline rounded-2xl block mx-auto"
           href="/resume.pdf"
           target="_blank"
         >
